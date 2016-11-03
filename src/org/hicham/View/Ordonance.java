0cd0414aef312a69a06d32becc26fac6d7fd12bc
Gherbi Hicham <gherbixhicham@gmail.com>
2016-11-03 10:29:24 +0100

    adding components to Ordonance view

--- src/org/hicham/View/Ordonance.java
+++ src/org/hicham/View/Ordonance.java
@@ -1,28 +1,51 @@
 package org.hicham.View;
 
 import java.awt.Color;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionListener;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+
+import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 
 public class Ordonance extends JPanel {
-
-	JButton btn = new JButton("dhfhff");
+	
+	JLabel nomMedlab= new JLabel("Nom De Medicament: ");
+	final DefaultComboBoxModel nomMedModel = new DefaultComboBoxModel();
+	final JComboBox nomMed = new JComboBox(nomMedModel);    
+	private JScrollPane nomMedListScrol = new JScrollPane(nomMed);
+	
+	JLabel situationLab= new JLabel("situation: ");
+    JTextField situation= new JTextField();
+	
+	
+	JButton ok = new JButton("Ok");
+	
 
 	public Ordonance() {
-		this.setLayout(null);
+		this.setLayout(new GridBagLayout());
 		this.setBackground(Color.blue);
-		this.add(btn);
-		btn.setBounds(600,150 , 100,20 );
+		
+		this.nomMedModel.addElement("");
+		AutoCompleteDecorator.decorate(nomMed);
+		nomMed.setSelectedIndex(0);
+		
+		this.add(ok);
+		ok.setBounds(600,150 , 100,20 );
 		this.setVisible(false);
 	}
 
 	public void addOrdonanceActionListener(ActionListener listener){
 
-		this.btn.addActionListener(listener);
+		this.ok.addActionListener(listener);
 	}
 	public JButton getBtn(){
-		return this.btn;
+		return this.ok;
 	}
 }
