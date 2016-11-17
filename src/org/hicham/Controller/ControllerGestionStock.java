f6d93dc704b8623cc177de2ed972219738955612
Gherbi Hicham <gherbixhicham@gmail.com>
2016-11-17 11:57:01 +0100

    adding to gestionStock view and creating its controller

---
+++ src/org/hicham/Controller/ControllerGestionStock.java
@@ -0,0 +1,9 @@
+package org.hicham.Controller;
+
+public class ControllerGestionStock {
+	
+	
+	//This is for getting buttons from menu
+	//addingquantity.getPopmenu().show(addingquantity.getChoixBtn(), addingquantity.getChoixBtn().getBounds().x-312, addingquantity.getChoixBtn().getBounds().y-65 + addingquantity.getChoixBtn().getBounds().height);
+
+}
--- src/org/hicham/View/GestionStockView.java
+++ src/org/hicham/View/GestionStockView.java
@@ -1,35 +1,81 @@
 package org.hicham.View;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 
 public class GestionStockView extends JPanel{
-	
+	// ajout qte
 	JLabel nomProduitLab= new JLabel("Produit: ");
 	
 	final DefaultComboBoxModel produitModel = new DefaultComboBoxModel();
 	final JComboBox produitCombo = new JComboBox(produitModel);    
 	private JScrollPane produitListScrol = new JScrollPane(produitCombo);
 	
+    private JButton choixBtn = new JButton();
+	private JPopupMenu  popmenu= new JPopupMenu();
+	JMenuItem modifieItem = new JMenuItem("Modifie");
+	JMenuItem supItem = new JMenuItem("Supprimé");
+	
+	JLabel qteAjoutLab= new JLabel("Qte a Ajouté: ");
+	JTextField qteAjout= new JTextField();
+	
+	JButton ajoutQte= new JButton("Ajouté qte");
+	//soustraction de qte 
+	
+	//ajout Produit
 	JLabel produitAjoutLab= new JLabel("Produit: ");
 	JTextField textProduitAjout= new JTextField();
 	
 	JLabel prixProduitAjout= new JLabel("Prix: ");
 	JTextField textPrixAjout= new JTextField();
-
-
 	
+	public  DefaultTableModel dt;
+	public  JTable produitsTab;
+	private JScrollPane scrolPane;
+
+
 	public GestionStockView(){
+		
 		this.setLayout(null);
 		this.setBackground(Color.decode("#d2fdf9"));
 		
+		this.produitModel.addElement("");
+		AutoCompleteDecorator.decorate(produitCombo);
+		produitCombo.setSelectedIndex(0);
 		
+		produitsTab =new JTable(dt);
+		produitsTab.setPreferredScrollableViewportSize(new Dimension(500,50));
+		produitsTab.setFillsViewportHeight(true);
+		scrolPane= new JScrollPane(produitsTab);
+		
+		
+		this.add(nomProduitLab);
+		this.add(produitCombo);
+		this.add(produitAjoutLab);
+		this.add(textProduitAjout);
+		this.add(prixProduitAjout);
+		this.add(textPrixAjout);
+		this.add(nomProduitLab);
+		this.add(choixBtn);
+		this.popmenu.add(modifieItem);
+		this.popmenu.add(supItem);
+	
 	}
+	
+	
 
 }
