0a1e308cd08b07ba90d6ea47ef071a4000817280
Gherbi Hicham <gherbixhicham@gmail.com>
2016-11-26 12:22:18 +0100

    adding act annotation class

--- src/org/hicham/Model/Act.java
+++ src/org/hicham/Model/Act.java
@@ -14,5 +14,36 @@ public class Act {
 	@GeneratedValue(strategy= GenerationType.AUTO)
 	@Column (name="id" ,nullable=false)
 	int id;
+	@Column (name="act" ,nullable=false)
+	String act;
+	@Column (name="payement" ,nullable=false)
+    double payement;
+	public Act(String act, double payement){
+		this.payement=payement;
+		this.act= act;
+	}
+	public Act(){
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAct() {
+		return act;
+	}
+	public void setAct(String act) {
+		this.act = act;
+	}
+	public double getPayement() {
+		return payement;
+	}
+	public void setPayement(double payement) {
+		this.payement = payement;
+	}
+	
+
+	
 	
 }
